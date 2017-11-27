09351115dc7f7a5122fa81dd3b4bfbacdfc40582
howiekick <howardna317@gmail.com>
2017-11-27 14:50:26 -0800

    [fix] resolves merge conflict

--- client/src/common/Footer.jsx
+++ client/src/common/Footer.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      <h1> FOOTER </h1>
-    </div>
+    <footer>
+      <div className="container-fluid">
+        <div className="row">
+            
+          <div className="col-md-3">
+            <p>Disclaimer: This website includes information about legal issues and legal developments. Such materials are for informational purposes only and should not be taken as legal advice. You should contact an attorney for advice on specific legal matters. The use of this website DOES NOT CREATE AN ATTORNEY-CLIENT RELATIONSHIP between you and the Law Offices of Alex Cha and Associates nor the individual attorneys featured on the site. By using this website you accept this disclaimer in full.</p>
+          </div>
+            
+          <div className="col-md-3">
+          </div>
+            
+          <div className="col-md-3">
+          </div>
+            
+        </div>
+      </div>
+    </footer>
   );
 };
 
